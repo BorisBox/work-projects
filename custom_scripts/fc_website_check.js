b4059e34ce18240165382a79ec85220a4d99bfb4
BorisBox <tutorijalisrbijaboris@gmail.com>
2022-08-16 20:03:01 +0200

    Update fc_website_check.js

--- custom_scripts/fc_website_check.js
+++ custom_scripts/fc_website_check.js
@@ -40,7 +40,7 @@ $('input[type="email"]').one("click", function() {
     runReport()
 })
 
-//console.clear();
+console.clear();
 console.log('%cPlease click on the input field of the newsletter form in the footer (the one that usually says "email" or "Enter your Email"', 'background: #FDDA0D; color: #000000; padding: 5px; font-size: 18px;');
 
 function checkIfTrue(which) {
@@ -76,7 +76,7 @@ ccss_o = 'background: #382076; color: #ffffff; padding: 8px 20px; font-size: 18p
 ccss_t = 'background: #228C22; color: #000000; padding: 8px 20px; font-size: 18px; font-family: Verdana;';
 ccss_th = 'background: #73629F; color: #ffffff; padding: 8px 20px; font-size: 18px; font-family: Verdana;';
 ccss_f = 'background: #AF0606; color: #000000; padding: 8px 20px; font-size: 18px; font-family: Verdana;';
-//console.clear()
+console.clear()
 console.log('%cKlaviyo Onsite Script: %c' + klons,ccss_o ,checkIfTrue(1));
 console.log('%cViewed Product Metric: %c' + klvp,ccss_o ,checkIfTrue(2));
 console.log('%c   - Viewed Product Metric Type: %c' + klvpmt,ccss_th ,checkIfTrue(3));
